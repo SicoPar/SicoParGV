51fffab88cca71062e3c77790f9efd7a5378a4a7
lucianosicolo <luciano.sicolo@hotmail.com>
2023-06-18 23:09:01 -0300

    #3 Relacion Vehiculo-Usuario
    Se crea en la vista de usuario una tabla , la cual tiene una relacion de muchos vehiculos a un usuario

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/so/Usuario_vehiculos.java
@@ -0,0 +1,26 @@
+package domainapp.modules.simple.dom.so;
+
+
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.isis.applib.annotation.Collection;
+import org.apache.isis.applib.annotation.CollectionLayout;
+
+import lombok.RequiredArgsConstructor;
+
+@Collection                                             
+@CollectionLayout(defaultView = "table")
+@RequiredArgsConstructor                                
+public class Usuario_vehiculos {                            
+
+    private final Usuario usuario;                    
+
+    public List<Vehiculo> coll() {
+        return VehiculoRepository.findByUsuario(usuario);  
+    }
+
+    @Inject VehiculoRepository VehiculoRepository;                
+}
